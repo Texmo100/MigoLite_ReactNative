e791645d249a969ad7d0d63bcc9a39841f3ee4cc
Texmo100 <isaac.ir@hotmail.com>
2021-07-28 16:10:48 -0700

    Login set-up, need to prepare and modify

--- App.js
+++ App.js
@@ -1,28 +1,58 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+// App.js
 
-const App = () => {
-  return(
-    <View style={styles.appContainer}>
-      <Text>Hey yui this is migo lite</Text>
-    </View>
-  )
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Signup"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#3740FE',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen 
+        name="Signup" 
+        component={Signup} 
+        options={{ title: 'Signup' }}
+      />       
+      <Stack.Screen 
+        name="Login" 
+        component={Login} 
+        options={
+          {title: 'Login'},
+          {headerLeft: null} 
+        }
+      />
+      <Stack.Screen 
+       name="Dashboard" 
+       component={Dashboard} 
+       options={
+         { title: 'Dashboard' },
+         {headerLeft: null} 
+       }
+      />
+    </Stack.Navigator>
+  );
 }
 
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
-
-export default App
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
---
+++ database/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDJglit49oiX_l0TLw9EzCjQoUigox5dPc",
+    authDomain: "migo-8e856.firebaseapp.com",
+    projectId: "migo-8e856",
+    storageBucket: "migo-8e856.appspot.com",
+    messagingSenderId: "95939663662",
+    appId: "1:95939663662:web:79c139c6670279e06dccbf",
+    measurementId: "G-NLH9DQV1E9"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
